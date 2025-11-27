5affb2c6008e7cb2427b0f60b5c1053064969f3e
JefersonFreitas27 <jheffygo@gmail.com>
2025-11-27 09:18:48 -0300

    atualizei o css

--- scripts.js
+++ scripts.js
@@ -2,8 +2,14 @@ const convertButton = document.querySelector(".convert-button")
 
 function convertValues(){
     const inputCurrency = document.querySelector(".input-currency").value
+    const currencyValueToConvert= document.querySelector(".currency-value-to-convert")
+    const currencyValueConverted= document.querySelector(".currency-value")
+    
     const dolarValue=5.36
     const convertvalue= inputCurrency/dolarValue
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {style:"currency", currency:"BRL"}).format(inputCurrency)
+    currencyValueConverted.innerHTML =  new Intl.NumberFormat("en-US", {style:"currency", currency:"USD"}).format( convertvalue)
     console.log( convertvalue)  
+    
 }
 convertButton.addEventListener("click",convertValues)
